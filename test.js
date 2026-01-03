e80eb66e34aedc11a727d1669dbf8cf8977e2662
[ Cassondra ] Roberts <castastrophe+github@hey.com>
2026-01-03 17:19:02 -0500

    chore: updating publishing rules and concat test

--- test.js
+++ test.js
@@ -1,15 +1,3 @@
-/*!
-Copyright 2023. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
-
 import mock from "mock-fs";
 
 import fs from "fs";
@@ -45,7 +33,7 @@ test("concatenate two files", async (t) => {
 					readFile("./expected/concat.md"),
 					"content matches",
 				);
-				t.assert(result.map, "sourcemap content matches");
+				t.assert(result.map, "content matches");
 			})
 			// No errors should be thrown, so fail the test if one is
 			.catch((error) => {
